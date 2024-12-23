6fa357327405cebd91a1f9ece70fbcc8b8d0cae7
MykolaKyian <kynunpodau2@gmail.com>
2024-12-23 15:00:23 -0330

    add in validation Body method trim()

--- dist/validators/postsValidations.js
+++ dist/validators/postsValidations.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.postBlogIdValidation = exports.postContentValidation = exports.postShortDescriptionValidation = exports.postTitleValidation = void 0;
 const express_validator_1 = require("express-validator");
 const blogsRepository_1 = require("../repository/blogsRepository");
-exports.postTitleValidation = (0, express_validator_1.body)('title').isString().withMessage('not sting').isLength({ min: 1, max: 30 }).withMessage('too sortly or more then 30 mathes');
-exports.postShortDescriptionValidation = (0, express_validator_1.body)('shortDescription').isString().withMessage('not sting').isLength({ min: 1, max: 100 }).withMessage('too sortly or more then 100 mathes');
-exports.postContentValidation = (0, express_validator_1.body)('content').isString().withMessage('not sting').isLength({ min: 1, max: 1000 }).withMessage('too sortly or more then 1000 mathes');
+exports.postTitleValidation = (0, express_validator_1.body)('title').isString().withMessage('not sting').trim().isLength({ min: 1, max: 30 }).withMessage('too sortly or more then 30 mathes');
+exports.postShortDescriptionValidation = (0, express_validator_1.body)('shortDescription').isString().withMessage('not sting').trim().isLength({ min: 1, max: 100 }).withMessage('too sortly or more then 100 mathes');
+exports.postContentValidation = (0, express_validator_1.body)('content').isString().withMessage('not sting').trim().isLength({ min: 1, max: 1000 }).withMessage('too sortly or more then 1000 mathes');
 exports.postBlogIdValidation = (0, express_validator_1.body)('blogId').isString().withMessage('not sting').custom((id) => {
     const fuondBlog = blogsRepository_1.blogRepository.getById(id);
     if (!fuondBlog) {
--- src/validators/postsValidations.ts
+++ src/validators/postsValidations.ts
@@ -3,11 +3,11 @@ import { blogRepository } from "../repository/blogsRepository"
 
 
 
-export const postTitleValidation = body('title').isString().withMessage('not sting').isLength({min:1,max:30}).withMessage('too sortly or more then 30 mathes')
+export const postTitleValidation = body('title').isString().withMessage('not sting').trim().isLength({min:1,max:30}).withMessage('too sortly or more then 30 mathes')
 
-export const postShortDescriptionValidation = body('shortDescription').isString().withMessage('not sting').isLength({min:1,max:100}).withMessage('too sortly or more then 100 mathes')
+export const postShortDescriptionValidation = body('shortDescription').isString().withMessage('not sting').trim().isLength({min:1,max:100}).withMessage('too sortly or more then 100 mathes')
 
-export const postContentValidation = body('content').isString().withMessage('not sting').isLength({min:1,max:1000}).withMessage('too sortly or more then 1000 mathes')
+export const postContentValidation = body('content').isString().withMessage('not sting').trim().isLength({min:1,max:1000}).withMessage('too sortly or more then 1000 mathes')
 
 export const postBlogIdValidation = body('blogId').isString().withMessage('not sting').custom((id)=>{
    const fuondBlog = blogRepository.getById(id)
