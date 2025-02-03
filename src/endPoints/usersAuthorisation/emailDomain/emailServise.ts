f9f8bf1ba0dfa3696b9333285b9a5cea328e32c5
MykolaKyian <96984183+NikolayNikolay@users.noreply.github.com>
2025-02-03 12:13:38 -0330

    Update emailServise.ts , replaced key, was text is thml

--- src/endPoints/usersAuthorisation/emailDomain/emailServise.ts
+++ src/endPoints/usersAuthorisation/emailDomain/emailServise.ts
@@ -15,7 +15,7 @@ export const emailServise ={
            from: '"Blog Platform" <modovod228@gmail.com>', // sender address
            to: `${email}`, // list of receivers
            subject: "Confimation Code", // Subject line
-           text:`<h1>Thank for your registration</h1>
+           html:`<h1>Thank for your registration</h1>
            <p>To finish registration please follow the link below:
               <a href='https://somesite.com/confirm-email?code=${confirmCode}'>complete registration</a>
            </p>` // plain text body
